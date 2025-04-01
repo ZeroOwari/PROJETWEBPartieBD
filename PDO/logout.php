b1b7a205e0582733d096eaf1e8f6ca9a2693145d
Clem <clementmagnier62@gmail.com>
2025-04-01 21:16:48 +0200

    push en retard

--- PDO/logout.php
+++ PDO/logout.php
@@ -1,23 +1,13 @@
 <?php
-
-session_start();
-
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['pseudo'])) 
-{
-    $_SESSION['pseudo'] = $_POST['pseudo'];
-}
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['motDePasse'])) 
-{
-    $_SESSION['motDePasse'] = $_POST['motDePasse'];
-}
-if (isset($_SESSION['statutAdmin'])) 
-{
-    echo "Debug: statutAdmin = " . htmlspecialchars($_SESSION['statutAdmin']);
-} else {
-    echo "Debug: statutAdmin is not set.";
-}
-
-
-
-
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['logout'])) 
+        {
+            session_start();
+            session_unset();
+            session_destroy();
+            header('Location: ../index.html'); // Redirect to the homepage
+            exit;
+        }
 ?>
+<form action="" method="post">
+    <button type="submit" name="logout">Logout</button>
+</form>
