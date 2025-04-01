ec8c24d9dd7b422cae85c3aea6a32a9321d40c45
fvancamp5 <theotimevancamp@gmail.com>
2025-04-02 00:24:40 +0200

    ca marchait deja en fait du coup juste fait la meme avec admin

---
+++ PDO/ConnexionAdmin.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+include("Admin.php");
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['email'])) 
+{
+    $email = $_POST['email'];
+}
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['password']) && $_POST['password']) 
+{
+    $password = $_POST['password'];
+}
+    
+    $db = new Admin('mysql:host=localhost;dbname=web4all', 'TOtime', 'Password0508');
+
+    if ($db->checkLogValidation([
+        'email' => $email,
+        'password' => $password,
+    ])) {
+        // Authentification réussie
+        $_SESSION['email'] = $email;
+    
+        // Redirection vers la page d'accueil
+        header("Location: accueil.html?login=success");
+        exit();
+    } else {
+        // Authentification échouée
+        echo "Email ou mot de passe incorrect.";
+    }
+?>
