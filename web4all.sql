70e6b04453170d7dfbfaeb56cf263b3a8918a629
fvancamp5 <theotimevancamp@gmail.com>
2025-04-01 22:16:59 +0200

    DB update

--- web4all.sql
+++ web4all.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le : lun. 31 mars 2025 à 21:41
+-- Généré le : mar. 01 avr. 2025 à 20:16
 -- Version du serveur : 9.1.0
 -- Version de PHP : 8.3.14
 
@@ -65,19 +65,12 @@ CREATE TABLE IF NOT EXISTS `ajout` (
   `ID-auteur` int NOT NULL COMMENT 'identifiant de l''auteur du changement'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
--- --------------------------------------------------------
-
 --
--- Structure de la table `cv`
+-- Déchargement des données de la table `ajout`
 --
 
-DROP TABLE IF EXISTS `cv`;
-CREATE TABLE IF NOT EXISTS `cv` (
-  `ID-CV` int NOT NULL AUTO_INCREMENT COMMENT 'Identifiant du cv d''un étudiant',
-  `ID-etudiant-CV` int NOT NULL COMMENT 'identifiant de l''etudiant a qui appartient le cv (clé étrangère table Etudiant)',
-  `CheminAcces-CV` varchar(255) NOT NULL COMMENT 'chemin d''acces vers le cv (local ou url)',
-  PRIMARY KEY (`ID-CV`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+INSERT INTO `ajout` (`ID-entreprise`, `ID-auteur`) VALUES
+(10, 1);
 
 -- --------------------------------------------------------
 
@@ -95,14 +88,17 @@ CREATE TABLE IF NOT EXISTS `entreprise` (
   `Note-entreprise` int DEFAULT NULL COMMENT 'moyenne des evaluations de l''entreprise',
   `CheminImage-entreprise` varchar(255) NOT NULL COMMENT 'chemin d''accées vers le logo de l''entreprise',
   PRIMARY KEY (`ID-entreprise`)
-) ENGINE=MyISAM AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `entreprise`
 --
 
 INSERT INTO `entreprise` (`ID-entreprise`, `Nom-entreprise`, `Description-entreprise`, `Email-entreprise`, `Telephone-entreprise`, `Note-entreprise`, `CheminImage-entreprise`) VALUES
-(3, 'Thales', 'Cybersecurite et IA', 'test@thales.com', '0223568978', 4, 'c:/wamp64/www/Thales.jpg');
+(3, 'Thales', 'Cybersecurite et IA', 'test@thales.com', '0223568978', 4, 'c:/wamp64/www/Thales.jpg'),
+(8, 'Ankama', 'Société de développement de jeux vidéo', 'societe@ankama.com', '0123456789', 5, 'images/ankama.png'),
+(9, 'Ankama', 'Société de développement de jeux vidéo', 'societe@ankama.com', '0123456789', 5, 'images/ankama.png'),
+(10, 'Ankama', 'Société de développement de jeux vidéo', 'societe@ankama.com', '0123456789', 5, 'images/ankama.png');
 
 -- --------------------------------------------------------
 
@@ -117,19 +113,20 @@ CREATE TABLE IF NOT EXISTS `etudiant` (
   `Nom-etudiant` varchar(60) NOT NULL COMMENT 'Nom de l''etudiant',
   `Email-etudiant` varchar(255) NOT NULL COMMENT 'Mail de log compte etudiant',
   `MDP-etudiant` varchar(60) NOT NULL COMMENT 'Mot de passe acces au compte etudiant',
-  `Telephone-etudiant` varchar(15) DEFAULT NULL COMMENT 'Num de téléphone étudiant',
-  `DateNaissance-etudiant` date DEFAULT NULL COMMENT 'date de naissance de l''etudiant',
-  `Chemin-CV` varchar(255) NOT NULL COMMENT 'Chemin d''acces vers le cv',
-  `ID-promotion-etudiant` int NOT NULL COMMENT 'identifiant de la promotion (clé étrangère table Promotion)',
+  `Telephone-etudiant` varchar(15) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'Num de téléphone étudiant',
+  `DateNaissance-etudiant` date NOT NULL COMMENT 'date de naissance de l''etudiant',
+  `Chemin-CV` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci DEFAULT NULL COMMENT 'Chemin d''acces vers le cv',
+  `ID-promotion-etudiant` int DEFAULT NULL COMMENT 'identifiant de la promotion (clé étrangère table Promotion)',
   PRIMARY KEY (`ID-etudiant`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `etudiant`
 --
 
 INSERT INTO `etudiant` (`ID-etudiant`, `Prenom-etudiant`, `Nom-etudiant`, `Email-etudiant`, `MDP-etudiant`, `Telephone-etudiant`, `DateNaissance-etudiant`, `Chemin-CV`, `ID-promotion-etudiant`) VALUES
-(1, 'Totime', 'VC', 'TVC@example.com', '$2y$10$IUFBma2KFdtrOkJwwtlu6.HHM8nEcDmmmV5QqTBIVHZCUD5e.Hnoy', '0631569513', '2005-08-05', '1', 1);
+(1, 'Totime', 'VC', 'TVC@example.com', '$2y$10$IUFBma2KFdtrOkJwwtlu6.HHM8nEcDmmmV5QqTBIVHZCUD5e.Hnoy', '0631569513', '2005-08-05', '1', 1),
+(2, 'Théotime', 'Van Camp', 'theotime.vancamp@icloud.com', '$2y$10$LI1g034PkWXoj8Pn9tM95.Rfwn3KTp6Kzt.SxAFpaG5nESZLZR8..', NULL, '2005-08-05', NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -160,19 +157,23 @@ CREATE TABLE IF NOT EXISTS `offrestage` (
   `Secteur-offre` varchar(60) NOT NULL COMMENT 'secteur d''activité de l''offre',
   `Localisation-offre` varchar(255) NOT NULL COMMENT 'position géographique de l''offre (lien vers API)',
   `ID-entreprise` int NOT NULL COMMENT 'Identifiant de l''entreprise où se deroule l''offre',
+  `Type-offre` varchar(60) NOT NULL COMMENT 'CDD, CDI, Stage ou alternance precise le type de contrat',
   PRIMARY KEY (`ID-offre`)
-) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `offrestage`
 --
 
-INSERT INTO `offrestage` (`ID-offre`, `Nom-offre`, `Description-offre`, `Competences-offre`, `Debut-offre`, `Fin-offre`, `Secteur-offre`, `Localisation-offre`, `ID-entreprise`) VALUES
-(1, 'Développeur Web', 'Développement de sites web et applications', 'HTML, CSS, JavaScript, PHP', '2025-04-01', '2025-06-30', 'Informatique', 'Paris', 101),
-(2, 'Analyste Marketing', 'Analyse des données de marché et stratégie', 'SEO, Google Analytics, Data Analysis', '2025-05-01', '2025-08-31', 'Marketing', 'Lille', 102),
-(3, 'Ingénieur Logiciel', 'Développement et maintenance de logiciels', 'Python, Java, C++', '2025-03-01', '2025-09-30', 'Technologie', 'Lyon', 103),
-(4, 'Consultant RH', 'Gestion et amélioration des processus RH', 'Communication, Management, Recrutement', '2025-04-15', '2025-07-15', 'Ressources Humaines', 'Marseille', 104),
-(5, 'Chef de Projet', 'Planification et supervision des projets', 'Gestion de projet, Leadership, Agile', '2025-01-01', '2025-12-31', 'Management', 'Bordeaux', 105);
+INSERT INTO `offrestage` (`ID-offre`, `Nom-offre`, `Description-offre`, `Competences-offre`, `Debut-offre`, `Fin-offre`, `Secteur-offre`, `Localisation-offre`, `ID-entreprise`, `Type-offre`) VALUES
+(1, 'Développeur Web', 'Développement de sites web et applications', 'HTML, CSS, JavaScript, PHP', '2025-04-01', '2025-06-30', 'Informatique', 'Paris', 101, ''),
+(2, 'Analyste Marketing', 'Analyse des données de marché et stratégie', 'SEO, Google Analytics, Data Analysis', '2025-05-01', '2025-08-31', 'Marketing', 'Lille', 102, ''),
+(3, 'Ingénieur Logiciel', 'Développement et maintenance de logiciels', 'Python, Java, C++', '2025-03-01', '2025-09-30', 'Technologie', 'Lyon', 103, ''),
+(4, 'Consultant RH', 'Gestion et amélioration des processus RH', 'Communication, Management, Recrutement', '2025-04-15', '2025-07-15', 'Ressources Humaines', 'Marseille', 104, ''),
+(5, 'Chef de Projet', 'Planification et supervision des projets', 'Gestion de projet, Leadership, Agile', '2025-01-01', '2025-12-31', 'Management', 'Bordeaux', 105, ''),
+(6, 'Developpeur PHP', 'Developpeur PHP pour un stage de 6 mois', 'PHP, MySQL, HTML, CSS', '2025-01-01', '2025-06-30', 'Informatique', 'Lille', 3, ''),
+(13, 'Chef Pentester', 'Test de sécurité', 'Python, C++', '2025-01-01', '2025-06-30', 'Informatique', 'Lyon', 1, ''),
+(7, 'Développeur Web', 'Développement de sites web et applications', 'HTML, CSS, JavaScript, PHP', '0000-00-00', '0000-00-00', 'Informatique', 'Paris', 1, 'Stage');
 
 -- --------------------------------------------------------
 
@@ -188,14 +189,15 @@ CREATE TABLE IF NOT EXISTS `pilotepromo` (
   `Email-pilote` varchar(255) NOT NULL COMMENT 'Email de log pilote',
   `MDP-pilote` varchar(60) NOT NULL COMMENT 'Mot de passe compte pilote',
   PRIMARY KEY (`ID-pilote`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `pilotepromo`
 --
 
 INSERT INTO `pilotepromo` (`ID-pilote`, `Prenom-pilote`, `Nom-pilote`, `Email-pilote`, `MDP-pilote`) VALUES
-(1, 'Clement', 'Magnier', 'CM@example.com', '$2y$10$NvVXW7UgfMceNdlOcV4GUOuoSPONd8uGzMBzo89OcZ47OalDVGTqi');
+(1, 'Clement', 'Magnier', 'CM@example.com', '$2y$10$NvVXW7UgfMceNdlOcV4GUOuoSPONd8uGzMBzo89OcZ47OalDVGTqi'),
+(2, 'Clément', 'Magnier', 'clement.magnier@viacesi.fr', '$2y$10$nmELEaiyMFzdaT07dVXrGuEQ.PhlJ9TTe.p8QTTLsLxmCiK8XFp4.');
 
 -- --------------------------------------------------------
 
@@ -223,7 +225,14 @@ CREATE TABLE IF NOT EXISTS `promotion` (
   `Debut-promo` date NOT NULL COMMENT 'année de début de la promo',
   `Fin-promo` date NOT NULL COMMENT 'année de fin de la promo',
   PRIMARY KEY (`ID-promo`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `promotion`
+--
+
+INSERT INTO `promotion` (`ID-promo`, `Nom-promo`, `Debut-promo`, `Fin-promo`) VALUES
+(1, 'Promo Test', '2023-10-01', '2023-12-31');
 
 -- --------------------------------------------------------
 
@@ -236,6 +245,16 @@ CREATE TABLE IF NOT EXISTS `publication` (
   `ID-offre` int NOT NULL COMMENT 'Identifiant de l''offre publiée',
   `ID-auteur` int NOT NULL COMMENT 'Identifiant de l''auteur de la publication'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Déchargement des données de la table `publication`
+--
+
+INSERT INTO `publication` (`ID-offre`, `ID-auteur`) VALUES
+(7, 1),
+(8, 1),
+(12, 1),
+(13, 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
